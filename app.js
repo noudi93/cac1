420a6bdec0b0091ae57a186013b163b217886390
Timothy Sealy <timothy.sealy@gmail.com>
2018-09-23 20:58:54 +0200

    Added “bucket” route which can be used to list objects in a bucket (using ?name=<bucketname>).

--- app.js
+++ app.js
@@ -2,13 +2,36 @@ var express = require('express')
 var app = express()
 var util = require('util')
 var os = require('os')
+var AwsS3 = require ('aws-sdk/clients/s3')
 
 const PORT = process.env.PORT || 3000
 const HOSTNAME = os.hostname()
 
 app.get('/', function (req, res) {
-  res.send(util.format('Welcome to %s the current time is %d.', HOSTNAME, new Date())) 
+  res.send(util.format('Welcome to %s the current time is %d.', HOSTNAME, new Date()))
 })
+
+app.get('/bucket', function (req, res) {
+  var bucketname = req.query.name
+  var s3 = new AwsS3({apiVersion: '2006-03-01'})
+  const s3params = {
+    Bucket: bucketname,
+    MaxKeys: 20,
+    Delimiter: '/',
+  }
+  s3.listObjectsV2(s3params, function(err, data) {
+   if (err) {
+     // an error occurred
+     console.log(err, err.stack)
+     res.send("An error has occurred. Don't forget to specify bucket name: http://host/bucket?name=\'bucketname\'.")
+   } else {
+     // successful response
+     console.log(data)
+     res.send(data)
+   }
+ })
+})
+
 
 var server = app.listen(PORT, function() {
   console.log('Webserver started on host: ', HOSTNAME)
